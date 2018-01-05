5e4611be56e277267fd9f3cbd561efd2fee3e3b7
Mike van Nieuwburg <d215937@edu.rocwb.nl>
2018-01-05 23:58:54 +0100

    Added Service and currency class for gathering and changing objects

---
+++ src/CryptoManager/CryptoManager/Core/Data/Service.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CryptoManager.Core.Data
+{
+    class Service
+    {
+        public static async Task<dynamic> getData(string query)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(query);
+            dynamic data = null;
+            if (response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+            return data;
+        }
+    }
+}
---
+++ src/CryptoManager/CryptoManager/Core/Worker.cs
@@ -0,0 +1,18 @@
+﻿using CryptoManager.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoManager.Core
+{
+    class Worker
+    {
+        public static async Task<Currency> GetCurrency(string coinId)
+        {
+            string query = $"https://api.coinmarketcap.com/v1/ticker/{coinId}/";
+            dynamic results = await Service.getData(query).ConfigureAwait(false);
+
+        }
+    }
+}
