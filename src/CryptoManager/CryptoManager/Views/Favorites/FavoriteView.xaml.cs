225b82fb8962ea99147a0119945836b015ad65ac
Mike van Nieuwburg <d215937@edu.rocwb.nl>
2018-01-07 06:58:20 +0100

    Changed Location of testing

--- src/CryptoManager/CryptoManager/Views/Favorites/FavoriteView.xaml.cs
+++ src/CryptoManager/CryptoManager/Views/Favorites/FavoriteView.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CryptoManager.Core.Data;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CryptoManager.Views.Favorites
@@ -12,22 +6,9 @@ namespace CryptoManager.Views.Favorites
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class FavoriteView : ContentPage
 	{
-		public FavoriteView()
-		{
-			InitializeComponent();
-            AllowDelegates();
-        }
-
-        private void AllowDelegates()
+        public FavoriteView()
         {
-            Button Btn = this.FindByName<Button>("btn");
-            Btn.Clicked += Btn_Clicked;
-        }
-
-        private async void Btn_Clicked(object sender, EventArgs e)
-        {
-            Currency cur = await Core.Worker.GetCurrency("bitcoin");
-            await DisplayAlert("Title", cur.Price_USD, "Cancel");
+            InitializeComponent();
         }
     }
 }
--- src/CryptoManager/CryptoManager/Views/Search/SearchView.xaml.cs
+++ src/CryptoManager/CryptoManager/Views/Search/SearchView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CryptoManager.Core.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,19 @@ namespace CryptoManager.Views.Search
 	{
 		public SearchView ()
 		{
-			InitializeComponent ();
-		}
-	}
+			InitializeComponent (); AllowDelegates();
+        }
+
+        private void AllowDelegates()
+        {
+            Button Btn = this.FindByName<Button>("btn");
+            Btn.Clicked += Btn_Clicked;
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            Currency cur = await Core.Worker.GetCurrency("bitcoin");
+            await DisplayAlert("Title", cur.Price_USD, "Cancel");
+        }
+    }
 }
