7b4965bdaa725a60ea0809a3940aa524d7e72f00
Mike van Nieuwburg <d215937@edu.rocwb.nl>
2018-01-07 02:24:59 +0100

    Added functional version

--- src/CryptoManager/CryptoManager/Core/Worker.cs
+++ src/CryptoManager/CryptoManager/Core/Worker.cs
@@ -11,8 +11,32 @@ namespace CryptoManager.Core
         public static async Task<Currency> GetCurrency(string coinId)
         {
             string query = $"https://api.coinmarketcap.com/v1/ticker/{coinId}/";
-            dynamic results = await Service.getData(query).ConfigureAwait(false);
+            dynamic results = await Service.getData(query).ConfigureAwait(true);
+            if (results["id"] != null)
+            {
+                Currency cur = new Currency();
+                cur.ID = results["id"];
+                cur.Title = results["name"];
+                cur.Symbol = results["symbol"];
+                cur.CMCRank = results["rank"];
+                cur.Price_USD = results["price_usd"];
+                cur.Price_BTC = results["price_btc"];
+                cur.Volume = results["24h_volume_usd"];
+                cur.MCUsd = results["market_cap_usd"];
+                cur.Available = results["available_supply"];
+                cur.Total = results["total_supply"];
+                cur.Max = results["max_supply"];
+                cur.PercChangeHour = results["percent_change_1h"];
+                cur.PercChangeDay = results["percent_change_24h"];
+                cur.PercChangeWeek = results["percent_change_7d"];
+                cur.LastUpdate = results["last_updated"];
 
+                return cur;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
--- src/CryptoManager/CryptoManager/Views/Favorites/FavoriteView.xaml.cs
+++ src/CryptoManager/CryptoManager/Views/Favorites/FavoriteView.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CryptoManager.Core.Data;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,18 @@ namespace CryptoManager.Views.Favorites
 		public FavoriteView()
 		{
 			InitializeComponent();
+            AllowDelegates();
         }
-	}
+
+        private void AllowDelegates()
+        {
+            Button Btn = this.FindByName<Button>("btn");
+            Btn.Clicked += Btn_Clicked;
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            Currency cur = await Core.Worker.GetCurrency("bitcoin");
+        }
+    }
 }
