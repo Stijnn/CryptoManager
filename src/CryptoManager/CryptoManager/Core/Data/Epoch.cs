5e9beac31d31364a35438c999264a7724e8d7fef
Mike van Nieuwburg <d215937@edu.rocwb.nl>
2018-01-07 08:54:56 +0100

    Added Epoch for converting unix timestamp to date

---
+++ src/CryptoManager/CryptoManager/Core/Data/Epoch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CryptoManager.Core.Data
+{
+    class Epoch
+    {
+        public static async Task<string> Timestamp(double unixSec)
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(unixSec);
+            return $"{epoch.ToLongTimeString()} {epoch.ToLongDateString()}";
+        }
+    }
+}
